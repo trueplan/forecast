435a4e862374198c639128d9fb32817d7ed7bd5a
Rich Bachman <rb@richbachman.com>
2021-11-10 16:51:56 -0700

    chore(theme): load Inter font from s3 bucket

--- packages/theme/src/styles/fonts.ts
+++ packages/theme/src/styles/fonts.ts
@@ -9,6 +9,6 @@ export const globalFonts = globalCss({
     fontWeight: "100 900",
     fontDisplay: "swap",
     fontStyle: "oblique 0deg 10deg",
-    src: 'url("static/media/Inter.var.woff2?v=3.19") format("woff2")',
+    src: 'url("https://bucketeer-f0258cad-6588-40ad-ac38-7a3b82df05e2.s3.amazonaws.com/public/fonts/Inter.var.woff2?v=3.19") format("woff2")',
   },
 });
