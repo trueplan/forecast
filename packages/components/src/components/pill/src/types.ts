b1c6b8e2da15e8bb3c616482b0ee095894852759
Rich Bachman <rb@richbachman.com>
2021-11-15 16:24:12 -0700

    fix(pill): change to use inline-block styles

--- packages/components/src/components/pill/src/types.ts
+++ packages/components/src/components/pill/src/types.ts
@@ -1,6 +1,6 @@
 export interface PillProps
   extends Omit<React.HTMLAttributes<HTMLSpanElement>, "css"> {
-  children: string;
+  children: React.ReactNode;
   color?: "green" | "blue" | "lilac" | "grey" | "red";
   fontFamily?: "roboto";
   size?: "small" | "large";
