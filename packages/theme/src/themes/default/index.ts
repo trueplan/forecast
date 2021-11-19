1e39e12cc8b4a3f1676504a1b60538b1706973ce
Rich Bachman <rb@richbachman.com>
2021-11-19 10:12:26 -0700

    chore(theme): export keyframes from Stitches

--- packages/theme/src/themes/default/index.ts
+++ packages/theme/src/themes/default/index.ts
@@ -4,7 +4,7 @@ import type * as Stitches from "@stitches/react";
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore - Jest complains about types here, but everything seems to render properly.
 // Could be something in our jest config or tsconfig.
-export const { styled, css, theme } = createStitches({
+export const { styled, css, theme, keyframes } = createStitches({
   prefix: "tp",
   theme: {
     colors: {
