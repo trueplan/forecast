a03a6d40f8b84113c69cd3c5f83de173728d70e0
Rich Bachman <rb@richbachman.com>
2022-02-16 14:13:29 -0700

    TRUE-627 | feat(stat): add non-button stat container (#48)
    * feat(stat): add non-button stat container
    
    * chore(stat): update story to reflect all parts

--- packages/components/src/components/stat/__tests__/stat.test.tsx
+++ packages/components/src/components/stat/__tests__/stat.test.tsx
@@ -1,6 +1,12 @@
 import * as React from "react";
 import { render, screen } from "@testing-library/react";
-import { Stat, StatName, StatNumber, StatInformation } from "../src";
+import {
+  Stat,
+  StatButton,
+  StatName,
+  StatNumber,
+  StatInformation,
+} from "../src";
 
 describe("Stat", () => {
   it("should render a stat container", () => {
@@ -9,16 +15,22 @@ describe("Stat", () => {
     expect(renderedStat).not.toBeNull();
   });
 
+  it("should render a stat button container", () => {
+    render(<StatButton>stat</StatButton>);
+    const renderedStatButton = screen.getByRole("button");
+    expect(renderedStatButton).not.toBeNull();
+  });
+
   it("should render a stat name", () => {
     render(<StatName data-testid="stat-name">stat-name</StatName>);
-    const renderedStat = screen.getByTestId("stat-name");
-    expect(renderedStat).not.toBeNull();
+    const renderedStatName = screen.getByTestId("stat-name");
+    expect(renderedStatName).not.toBeNull();
   });
 
   it("should render a stat number", () => {
     render(<StatNumber data-testid="stat-number">stat-number</StatNumber>);
-    const renderedStat = screen.getByTestId("stat-number");
-    expect(renderedStat).not.toBeNull();
+    const renderedStatNumber = screen.getByTestId("stat-number");
+    expect(renderedStatNumber).not.toBeNull();
   });
 
   it("should render a stat information", () => {
@@ -27,7 +39,7 @@ describe("Stat", () => {
         stat-information
       </StatInformation>
     );
-    const renderedStat = screen.getByTestId("stat-information");
-    expect(renderedStat).not.toBeNull();
+    const renderedStatInformation = screen.getByTestId("stat-information");
+    expect(renderedStatInformation).not.toBeNull();
   });
 });
--- packages/components/src/components/stat/stories/index.stories.tsx
+++ packages/components/src/components/stat/stories/index.stories.tsx
@@ -1,7 +1,15 @@
 import * as React from "react";
+import { Box } from "../../../primitives/box";
 import { Pill } from "../../pill";
 import { ProgressBar } from "../../progress";
-import { Stat, StatName, StatNumber, StatInformation } from "../src";
+import { Status } from "../../status";
+import {
+  Stat,
+  StatButton,
+  StatName,
+  StatNumber,
+  StatInformation,
+} from "../src";
 
 // eslint-disable-next-line import/no-default-export
 export default {
@@ -11,6 +19,14 @@ export default {
 
 export const Default: React.FC = () => (
   <Stat>
+    <StatName>Stat Name</StatName>
+    <StatNumber>100</StatNumber>
+    <StatInformation>More information</StatInformation>
+  </Stat>
+);
+
+export const WithPadding: React.FC = () => (
+  <Stat padding="space40">
     <StatName>Stat Name</StatName>
     <StatNumber>100</StatNumber>
     <StatInformation>More information</StatInformation>
@@ -38,4 +54,38 @@ export const Progress: React.FC = () => (
       99%
     </Pill>
   </Stat>
+);
+
+export const StatButtonDefault: React.FC = () => (
+  <StatButton>
+    <StatName>Stat Name</StatName>
+    <StatNumber>100</StatNumber>
+    <StatInformation>More information</StatInformation>
+  </StatButton>
+);
+
+export const StatButtonPercentage: React.FC = () => (
+  <StatButton>
+    <StatName>Stat Name</StatName>
+    <StatNumber>100</StatNumber>
+    <StatInformation>More information</StatInformation>
+    <Pill color="green" size="small" fontFamily="roboto">
+      99%
+    </Pill>
+  </StatButton>
+);
+
+export const StatButtonProgress: React.FC = () => (
+  <StatButton>
+    <StatName>Stat Name</StatName>
+    <StatNumber>100</StatNumber>
+    <StatInformation>More information</StatInformation>
+    <ProgressBar value={50} />
+    <Box css={{ display: "flex", gap: "$25", alignItems: "center" }}>
+      <StatInformation>From 980k</StatInformation>
+      <Pill color="green" size="small" fontFamily="roboto">
+        99%
+      </Pill>
+    </Box>
+  </StatButton>
 );
