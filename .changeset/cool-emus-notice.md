8f46baa3c0af5287b6cc9d74c9386c7fd9ae0f87
Bryce Wellington <bryce@trueplan.io>
2022-05-03 11:15:43 -0700

    feat(avatar): adds bg color util

---
+++ .changeset/cool-emus-notice.md
@@ -0,0 +1,5 @@
+---
+"@trueplan/forecast-components": patch
+---
+
+[avatar]: adds helper function to generate an avatar background color using the length of a person's full name
--- packages/components/src/components/avatar/src/utils.ts
+++ packages/components/src/components/avatar/src/utils.ts
@@ -1,3 +1,5 @@
+import type { AvatarColors } from ".";
+
 export const getInitialsFromName = (fullname: string): string => {
   return fullname
     .split(" ")
@@ -9,3 +11,26 @@ export const getInitialsFromName = (fullname: string): string => {
       return previous;
     }, "");
 };
+
+const avatarColorOptions: AvatarColors[] = [
+  "blue",
+  "green",
+  "lilac",
+  "sky",
+  "yellow",
+  "red",
+  "royal",
+];
+
+/**
+ * Generates an avatar background color using the length of a person's full name
+ * @param name: person's full name
+ * @returns AvatarColors
+ *
+ * @example
+ * getAvatarColor("Pam Beesly")
+ * // returns "sky"
+ */
+
+export const getAvatarColor = (name: string): string =>
+  avatarColorOptions[name.length % avatarColorOptions.length];
