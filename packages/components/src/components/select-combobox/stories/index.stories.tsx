75c165fd47754e0a4e56b6a23a8d54e41be6f467
Rich Bachman <rb@richbachman.com>
2022-05-04 10:08:56 -0600

    chore(components): add story for open select combobox

--- packages/components/src/components/select-combobox/stories/index.stories.tsx
+++ packages/components/src/components/select-combobox/stories/index.stories.tsx
@@ -18,7 +18,6 @@ import { itemList as list } from "../../combobox/__fixtures__/item-list";
 // eslint-disable-next-line import/no-default-export
 export default {
   title: "Components/SelectCombobox",
-  // component: Example,
 };
 
 export const Default: React.FC = () => {
@@ -61,6 +60,61 @@ export const Default: React.FC = () => {
               {(props) => <SelectComboboxItem {...props} value={itemValue} />}
             </StyledComboboxItem>
           ))}
+        </SelectComboboxList>
+      </SelectComboboxPopover>
+    </>
+  );
+};
+
+export const DefaultOpen: React.FC = () => {
+  const combobox = useComboboxState({
+    list,
+    gutter: 4,
+    sameWidth: true,
+    visible: true,
+  });
+  // value and setValue shouldn't be passed to the select state because the
+  // select value and the combobox value are different things.
+  // value and setValue are in the combobox state
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { value, setValue, ...selectProps } = combobox;
+  const select = useSelectComboboxState({
+    ...selectProps,
+    defaultValue: "Apple",
+    visible: true,
+  });
+
+  // Resets combobox value when popover is collapsed
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // eslint-disable-next-line unicorn/consistent-destructuring
+  if (!select.mounted && combobox.value) {
+    combobox.setValue("");
+  }
+
+  return (
+    <>
+      <InputBox hasHover>
+        <SelectCombobox state={select} />
+      </InputBox>
+      <SelectComboboxPopover state={select} composite={false}>
+        <Box css={{ marginBottom: "$25" }}>
+          <InputBox>
+            <StyledCombobox
+              state={combobox}
+              autoSelect
+              placeholder="Search..."
+            />
+          </InputBox>
+        </Box>
+        <SelectComboboxList state={combobox}>
+          {
+            // eslint-disable-next-line sonarjs/no-identical-functions
+            combobox.matches.map((itemValue, i) => (
+              <StyledComboboxItem key={itemValue + i} focusOnHover>
+                {(props) => <SelectComboboxItem {...props} value={itemValue} />}
+              </StyledComboboxItem>
+            ))
+          }
         </SelectComboboxList>
       </SelectComboboxPopover>
     </>
