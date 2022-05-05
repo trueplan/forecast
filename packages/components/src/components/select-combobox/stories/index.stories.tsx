6e5763864ab8cc6ba274408a0b3f216993f070ce
Rich Bachman <rb@richbachman.com>
2022-05-05 15:32:04 -0600

    chore(components): add empty value select combobox story

--- packages/components/src/components/select-combobox/stories/index.stories.tsx
+++ packages/components/src/components/select-combobox/stories/index.stories.tsx
@@ -77,3 +77,38 @@ export const DefaultOpen: React.FC = () => {
     />
   );
 };
+
+export const EmptyValue: React.FC = () => {
+  const combobox = useComboboxState({
+    list,
+    gutter: 4,
+    sameWidth: true,
+  });
+  // value and setValue shouldn't be passed to the select state because the
+  // select value and the combobox value are different things.
+  // value and setValue are in the combobox state
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { value, setValue, ...selectProps } = combobox;
+  const select = useSelectComboboxState({
+    ...selectProps,
+    defaultValue: "",
+  });
+
+  // Resets combobox value when popover is collapsed
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // eslint-disable-next-line unicorn/consistent-destructuring
+  if (!select.mounted && combobox.value) {
+    combobox.setValue("");
+  }
+
+  const selectComboboxID = useUID();
+
+  return (
+    <SelectCombobox
+      comboboxState={combobox}
+      id={selectComboboxID}
+      label="Select a fruit"
+      selectState={select}
+    />
+  );
+};
