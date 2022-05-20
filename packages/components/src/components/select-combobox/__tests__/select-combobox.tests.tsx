b90e286ebbb405b13b06203a44afcbc93f0ca4e1
Rich Bachman <rb@richbachman.com>
2022-05-20 12:41:13 -0500

    test(components): add tests for select combobox

---
+++ packages/components/src/components/select-combobox/__tests__/select-combobox.tests.tsx
@@ -0,0 +1,47 @@
+import * as React from "react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { Default, DefaultOpen, EmptyValue } from "../stories/index.stories";
+
+describe("SelectCombobox", () => {
+  it("should render a SelectCombobox with a default value", () => {
+    render(<Default />);
+    expect(screen.getByRole("combobox")).toHaveTextContent("Apple");
+  });
+
+  it("should render a SelectCombobox with an empty value", () => {
+    render(<EmptyValue />);
+    expect(screen.getByRole("combobox")).toHaveTextContent("");
+  });
+
+  it("should render a dialog with a listbox", () => {
+    render(<Default />);
+    expect(screen.getByRole("dialog", { hidden: true })).toBeDefined();
+    expect(screen.getByRole("listbox", { hidden: true })).toBeDefined();
+  });
+
+  it("should render a combobox within the dialog", () => {
+    render(<DefaultOpen />);
+    expect(
+      screen.getByRole("combobox", { name: "", expanded: true })
+    ).toBeDefined();
+  });
+
+  it("should change the value when selecting an item from the listbox", () => {
+    render(<Default />);
+    expect(screen.getByRole("combobox")).toHaveTextContent("Apple");
+    const baconOptions = screen.getAllByRole("option", {
+      hidden: true,
+      name: "Bacon",
+    });
+    fireEvent.click(baconOptions[1]);
+    expect(screen.getByRole("combobox")).toHaveTextContent("Bacon");
+  });
+
+  it("should change the value when typing text in the combobox within the dialog", async () => {
+    render(<DefaultOpen />);
+    const comboboxes = screen.getAllByRole("combobox");
+    expect(comboboxes[0]).toHaveTextContent("Apple");
+    fireEvent.change(comboboxes[1], { target: { value: "Bacon" } });
+    expect(comboboxes[1]).toHaveDisplayValue("Bacon");
+  });
+});
