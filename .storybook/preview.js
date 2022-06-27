f70e930f34c7c6a05c1f7033a4a5027085c7a13f
Jay Nielsen <jayarnielsen@gmail.com>
2022-06-27 21:59:10 +0000

    fix: revert overzealous caret removal

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -13,6 +13,6 @@ export const decorators = [
 ];
 
 export const parameters = {
-  actions: { argTypesRegex: "on[A-Z].*" },
+  actions: { argTypesRegex: "^on[A-Z].*" },
   controls: { expanded: true },
 };
