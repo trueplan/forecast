85b63f707d5dd7c45d3671e318e22bcba0dc80a1
Rich Bachman <rb@richbachman.com>
2022-06-30 10:46:31 -0500

    docs(components): add pill doc comments

--- packages/components/src/components/pill/src/types.ts
+++ packages/components/src/components/pill/src/types.ts
@@ -1,6 +1,10 @@
 export interface PillProps extends React.HTMLAttributes<HTMLSpanElement> {
+  /** The valid HTML content of the pill. */
   children: React.ReactNode;
+  /** Specifies the color used for the pill. */
   color?: "green" | "blue" | "lilac" | "grey" | "red";
+  /** Specifies the font family used for the pill. */
   fontFamily?: "roboto";
+  /** Specifies the size used for the pill. */
   size?: "small" | "large";
 }
