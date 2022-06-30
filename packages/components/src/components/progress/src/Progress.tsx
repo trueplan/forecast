6efd838033e0c556b5879a87754b1caa4d2bc453
Rich Bachman <rb@richbachman.com>
2022-06-30 11:52:25 -0500

    docs(components): add progress doc comments

--- packages/components/src/components/progress/src/Progress.tsx
+++ packages/components/src/components/progress/src/Progress.tsx
@@ -2,6 +2,7 @@ import { StyledProgress } from "./styles";
 
 const Progress = StyledProgress;
 
+/** Displays an indicator showing the completion progress of a task, typically displayed as a progress bar. */
 Progress.displayName = "Progress";
 
 export { Progress };
--- packages/components/src/components/progress/src/types.ts
+++ packages/components/src/components/progress/src/types.ts
@@ -1,4 +1,6 @@
 export interface ProgressBarProps extends React.HTMLAttributes<HTMLDivElement> {
+  /** The progress value. */
   value: number;
+  /** The color of the progress bar. */
   color?: "blue" | "green" | "red" | "yellow";
 }
