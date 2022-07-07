57fb0a1ec0da73e16c4ca59a07e8d7d7026f1f92
Rich Bachman <rb@richbachman.com>
2022-07-07 14:33:57 -0500

    feat(components): add multiline capability to Truncate

--- packages/components/src/components/truncate/src/index.tsx
+++ packages/components/src/components/truncate/src/index.tsx
@@ -3,22 +3,29 @@ import { Box } from "../../../primitives/box";
 
 export interface TruncateProps
   extends React.HtmlHTMLAttributes<HTMLSpanElement> {
+  /** The text contents of Truncate. */
   children: React.ReactNode;
+  /** The accessible full text being truncated. This also displays on hover. */
   title: string;
+  /** Enables multiline truncation. Sets the number lines to truncate. */
+  lines?: number;
 }
 
+/** Truncate is a wrapper that shortens a string of text that overflows a defined area. */
 const Truncate = React.forwardRef<HTMLSpanElement, TruncateProps>(
-  ({ children, ...props }, ref) => {
+  ({ children, lines, ...props }, ref) => {
     return (
       <Box
         {...props}
         as="span"
         css={{
-          display: "inline-block",
+          display: lines ? "-webkit-box" : "inline-block",
           maxWidth: "100%",
           overflow: "hidden",
           textOverflow: "ellipsis",
-          whiteSpace: "nowrap",
+          whiteSpace: !lines ? "nowrap" : undefined,
+          WebkitBoxOrient: lines ? "vertical" : undefined,
+          WebkitLineClamp: lines ? lines : undefined,
         }}
         ref={ref}
       >
--- packages/components/src/components/truncate/stories/index.stories.tsx
+++ packages/components/src/components/truncate/stories/index.stories.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import type { ComponentStory, ComponentMeta } from "@storybook/react";
 import { Box } from "../../../primitives/box";
 import { Text } from "../../../primitives/text";
 import { Truncate } from "../src";
@@ -7,14 +8,33 @@ import { Truncate } from "../src";
 export default {
   title: "Components/Truncate",
   component: Truncate,
-};
+} as ComponentMeta<typeof Truncate>;
 
-export const Default = (): React.ReactNode => {
+export const Default: ComponentStory<typeof Truncate> = () => {
   return (
     <Box css={{ maxWidth: "150px" }}>
       <Text as="p">
         <Truncate title="Some very long text to truncate">
           Some very long text to truncate
+        </Truncate>
+      </Text>
+    </Box>
+  );
+};
+
+export const Multiline: ComponentStory<typeof Truncate> = () => {
+  return (
+    <Box css={{ maxWidth: "250px" }}>
+      <Text as="p">
+        <Truncate
+          title="Lorem ipsum dolor sit amet, consectetur. Lorem ipsum dolor sit amet,
+          consectetur. Lorem ipsum dolor sit amet, consectetur. Lorem ipsum
+          dolor sit amet, consectetur."
+          lines={3}
+        >
+          Lorem ipsum dolor sit amet, consectetur. Lorem ipsum dolor sit amet,
+          consectetur. Lorem ipsum dolor sit amet, consectetur. Lorem ipsum
+          dolor sit amet, consectetur.
         </Truncate>
       </Text>
     </Box>
